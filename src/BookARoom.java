cc0c4e323471a1cf57fb7ced0c01b408e80ba7a3
BanuTorun <68971649+BanuTorun@users.noreply.github.com>
2020-08-01 15:05:32 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("I love my kids and husband");
-        System.out.println("Aras","Tulpar","Ismail");
+        System.out.println("Aras");
             
 
 
