e7745046e0aff17a7f98fdb188fa6460a1c069a3
BanuTorun <banu.torun@hotmail.com>
2020-08-01 11:41:52 -0500

    add DayOfGit.java

--- src/DayOfGit.java
+++ src/DayOfGit.java
@@ -5,6 +5,10 @@ public class DayOfGit {
         String []arr={"Ismail,Aras,Tulpar,Banu"};
         System.out.println(Arrays.toString(arr));
         birthday("01/01/1977","13.09.2014","17.06.2016");
+        System.out.println("Aras is 6 years old");
+        System.out.println("Tulpar is 4 years old");
+        System.out.println("Ismail is 43 years old");
+
     }
     public static int birthday(String dob1, String dob2, String dob3){
         dob1="01.01.1977";
@@ -15,4 +19,6 @@ public class DayOfGit {
         result+= Integer.parseInt(dob1.substring(6));
                 return result;
     }
+
+
 }
