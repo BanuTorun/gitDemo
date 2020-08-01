9e70d2bb91eedcda61ef09994dda79f65c88553f
BanuTorun <banu.torun@hotmail.com>
2020-08-01 10:52:46 -0500

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,7 @@ public class HappyB20 {
 
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+
+        System.out.println("I love my family");
     }
 }
