c93036da04c2024b36cf8a7ce6da6fc802beb3f2
BanuTorun <68971649+BanuTorun@users.noreply.github.com>
2020-08-01 22:57:34 -0500

    Update EvenOdd.java

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -7,9 +7,9 @@ public class EvenOdd {
         int num=scan.nextInt();
 
         if(num%2==0){
-            System.out.println(num+" is even.");
+            System.out.println(num+" is even");
         }else{
-            System.out.println((num+" is odd."));
+            System.out.println((num+" is odd"));
         }
     }
 }
